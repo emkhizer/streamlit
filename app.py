514bb8caca174a8ed15e0cea2c9e4431016333c8
emkhizer <emkhizer007@gmail.com>
2025-02-21 20:34:20 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+# import streamlit as st
+
+
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+import random
+
+st.write('Hi!, My name is Khizer')
+st.title('Mera Pehla Web App')
+st.write('Yeh mera pehla Streamlit app hai!')
